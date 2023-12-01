ea89cd0271c14ee2f89dd8c544773cc7cbb52c38
Woojun Ro <woojunro0223@gmail.com>
2023-12-01 23:52:05 +0900

    [feat] Add video streaming to frontend

--- src/main/frontend/src/components/login/LoginButton.js
+++ src/main/frontend/src/components/login/LoginButton.js
@@ -1,18 +1,18 @@
-import {useGoogleLogin} from "@react-oauth/google";
+import { useGoogleLogin } from "@react-oauth/google";
 import styles from "./LoginFormContainer.module.css";
+import { GOOGLE_LOGIN_REDIRECT_URI } from "../../constant";
 
-export default function LoginButton(){
-    const login = useGoogleLogin({
-        ux_mode: "redirect",
-        redirect_uri: "http://localhost:8080/login/callback",
-        onSuccess: codeResponse => console.log(codeResponse),
-        flow: 'auth-code',
-    });
-    return(
-        <div className={styles.frame1}>
-            <div className={styles.frame2} onClick={()=>login()}>
-                Sign up google
-            </div>
-        </div>
-    )
+export default function LoginButton() {
+  const login = useGoogleLogin({
+    ux_mode: "redirect",
+    redirect_uri: GOOGLE_LOGIN_REDIRECT_URI,
+    flow: "auth-code",
+  });
+  return (
+    <div className={styles.frame1}>
+      <div className={styles.frame2} onClick={() => login()}>
+        Sign up google
+      </div>
+    </div>
+  );
 }
--- src/main/frontend/src/components/streaming/StreamingInfo.js
+++ src/main/frontend/src/components/streaming/StreamingInfo.js
@@ -1,32 +1,63 @@
 import styles from "./StreamingInfo.module.css";
 
-const StreamingInfo = () => {
-    return (
-        <div className={styles.streamingInfo}>
-            <div className={styles.frame}>
-                <h1 className={styles.leeMuJinContainer} id="title">
-                    <span>이무진(LEE Mu JIN)-북서울의 버스킹</span>
-                    <span className={styles.span}>{` `}</span>
-                </h1>
-                <div className={styles.frameClap}>
-                    <img
-                        className={styles.frameClapChild}
-                        id="clap"
-                        alt=""
-                        src="/rectangle-17@2x.png"
-                    /></div>
-            </div>
-            <div className={styles.frame1}>
-                <img
-                    className={styles.frameItem}
-                    id="user-profile"
-                    alt=""
-                    src="/rectangle-16@2x.png"
-                />
-                <div className={styles.userName}>user name</div>
-            </div>
+const StreamingInfo = ({ broadcast }) => {
+  const onStopButtonClick = () => {
+    const ok = window.confirm("정말 종료하시겠습니까?");
+
+    if (!ok) return;
+
+    fetch(
+      `https://api.video.wowza.com/api/v1.11/live_streams/${broadcast?.streamId}/stop`,
+      {
+        method: "PUT",
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_WOWZA_TOKEN}`,
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      fetch(
+        `https://api.video.wowza.com/api/v1.11/live_streams/${broadcast?.streamId}`,
+        {
+          method: "DELETE",
+          headers: {
+            Authorization: `Bearer ${process.env.REACT_APP_WOWZA_TOKEN}`,
+          },
+        }
+      );
+    });
+
+    fetch(`/api/v1/broadcasts/${broadcast?.id}/end`, {
+      method: "PUT",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(() => window.location.reload());
+  };
+
+  return (
+    <div className={styles.streamingInfo}>
+      <div className={styles.frame}>
+        <h1 className={styles.leeMuJinContainer} id="title">
+          <span>{broadcast?.title}</span>
+          <span className={styles.span}>{` `}</span>
+        </h1>
+        <div className={styles.frameClap}>
+          <button onClick={onStopButtonClick}>방송종료</button>
         </div>
-    );
+      </div>
+      <div className={styles.frame1}>
+        <img
+          className={styles.frameItem}
+          id="user-profile"
+          alt=""
+          src="/rectangle-16@2x.png"
+        />
+        <div className={styles.userName}>{broadcast?.host?.name}</div>
+      </div>
+    </div>
+  );
 };
 
 export default StreamingInfo;
